65c2b466fc09f50213a5c3947ec4bf51afbe2246
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2025-11-22 14:09:22 +0530

    cng

--- src/lib/instance.ts
+++ src/lib/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 const url = import.meta.env.MODE === 'development' ? import.meta.env.VITE_API_BACKEND_URL :  import.meta.env.VITE_LIVE_BACKEND_URL
-console.log("Backend URL:", url);
+console.log('ENVIRONMENT:', import.meta.env.MODE);
  const instance = axios.create({
-    baseURL:url!,
+    baseURL: import.meta.env.MODE ? url : import.meta.env.VITE_LIVE_BACKEND_URL ,
  })
  export default instance;
