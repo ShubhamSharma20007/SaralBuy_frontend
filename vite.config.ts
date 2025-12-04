43b4e52c885f88dc81f23bda792e5856df642bdd
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2025-12-04 20:13:08 +0530

    cng

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,14 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: true,
-    port: 5173,
-    hmr: mode !== 'production', // Disable HMR in production
-  },
   // build:{
   //   chunkSizeWarningLimit:1600
   // }
-}))
+})
