43f53c723c1d682ef6acc81f08c6c634f2fe8d79
ShubhamSharma20007 <shubhamsharma20007@gmail.com>
2025-12-25 22:43:14 +0530

    cng

--- src/lib/instance.ts
+++ src/lib/instance.ts
@@ -1,8 +1,7 @@
 import axios from "axios"
 export let url:any;
-// url = import.meta.env.MODE === 'development' ? import.meta.env.VITE_API_BACKEND_URL :  import.meta.env.import.meta.env.VITE_LIVE_BACKEND_URL
 //  url = import.meta.env.VITE_LIVE_BACKEND_URL
-url =  import.meta.env.VITE_PREFIX_URL
+url = import.meta.env.MODE === 'development' ? import.meta.env.VITE_API_BACKEND_URL  :  import.meta.env.VITE_PREFIX_URL
 console.log(import.meta.env.MODE)
  const instance = axios.create({
     baseURL: url! ,
